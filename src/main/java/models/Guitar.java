1f9efdd81b0a427ee6ff2f72c5aa8714d404b0e9
Angus Malloch <angusbass@hotmail.com>
2018-03-12 17:44:33 +0000

    Setup Guitar class.

--- src/main/java/models/Guitar.java
+++ src/main/java/models/Guitar.java
@@ -1,4 +1,39 @@
 package models;
 
 public class Guitar {
+    private String model;
+    private String manufacturer;
+    private int number_of_strings;
+
+    public Guitar() {}
+
+    public Guitar(String model, String manufacturer, int number_of_strings) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.number_of_strings = number_of_strings;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getNumber_of_strings() {
+        return number_of_strings;
+    }
+
+    public void setNumber_of_strings(int number_of_strings) {
+        this.number_of_strings = number_of_strings;
+    }
 }
