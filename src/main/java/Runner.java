f95698f9cd8861b29e544d952e0ea09fbdd8346c
Angus Malloch <angusbass@hotmail.com>
2018-03-12 22:14:27 +0000

    Added to the runner with find object, however delete by id is proving elusive. Keep getting a parm error about it not finding the id, yet it returns the correct id?!

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -17,14 +17,23 @@ public class Runner {
         Guitar guitar1 = new Guitar("Les Paul", "Gibson", 6, guitarist1);
         DBHelper.save(guitar1);
 
-        Guitar guitar2 = new Guitar("Corvette Hotrod", "Warwick", 5, guitarist1);
+        Guitar guitar2 = new Guitar("Corvette Hotrod", "Warwick", 5, guitarist2);
         DBHelper.save(guitar2);
+
+        DBHelper.deleteById("Guitar", guitar1.getId());
 
         guitar1.setManufacturer("Ibanez");
         DBHelper.update(guitar1);
 
+        DBHelper.delete(guitar1);
+
+        Guitar guitar = (Guitar)DBHelper.getObjectById("Guitar",guitar1.getId());
+
         List<Guitar> guitars = DBHelper.getAll("Guitar");
 
         List<Guitarist> guitarist = DBHelper.getAll("Guitarist");
+
+        List<Guitar> guitarsOfGuitarist1 = DBHelper.getGuitars(guitarist1.getId());
+
     }
 }
